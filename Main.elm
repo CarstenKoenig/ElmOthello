7d47c7a14234286806b48cfbb85bb9ec2996dbb7
Carsten König <carsten@gettingsharper.de>
2016-10-03 21:13:53 +0200

    refactored model-module into Othello and removed unneccessary functions

--- Main.elm
+++ Main.elm
@@ -10,8 +10,10 @@ import Array as Array
 import Svg exposing (Svg, svg)
 import Svg as Svg
 import Svg.Attributes as SvgAttr
+import Othello exposing (..)
 
 
+main : Program Never
 main =
     Html.App.program
         { init = ( init, Cmd.none )
@@ -26,116 +28,6 @@ type alias Model =
     , player : Stone
     , highlighted : Maybe Coord
     }
-
-
-type alias Board =
-    Array (Array Cell)
-
-
-type Cell
-    = Empty
-    | Occupied Stone
-
-
-type Stone
-    = Black
-    | White
-
-
-other : Stone -> Stone
-other stone =
-    case stone of
-        Black ->
-            White
-
-        White ->
-            Black
-
-
-type alias Coord =
-    { row : Int, col : Int }
-
-
-isValidMove : Board -> Stone -> Coord -> List Coord
-isValidMove board stone coord =
-    List.concatMap
-        (isValidMoveInDirection board stone coord)
-        [ ( -1, -1 ), ( -1, 0 ), ( -1, 1 ), ( 0, 1 ), ( 1, 1 ), ( 1, 0 ), ( 1, -1 ), ( 0, -1 ) ]
-
-
-isValidMoveInDirection : Board -> Stone -> Coord -> ( Int, Int ) -> List Coord
-isValidMoveInDirection board stone coord dir =
-    if not (isEmpty board coord) then
-        []
-    else
-        terminatedWith board stone dir (move dir coord) []
-
-
-terminatedWith : Board -> Stone -> ( Int, Int ) -> Coord -> List Coord -> List Coord
-terminatedWith board stone dir coord captured =
-    case occupied board coord of
-        Just stone' ->
-            if stone' == stone then
-                captured
-            else
-                terminatedWith board stone dir (move dir coord) (coord :: captured)
-
-        Nothing ->
-            []
-
-
-isEmpty : Board -> Coord -> Bool
-isEmpty board coord =
-    cellAt board coord == Just Empty
-
-
-occupied : Board -> Coord -> Maybe Stone
-occupied board coord =
-    cellAt board coord
-        `Maybe.andThen`
-            (\cell ->
-                case cell of
-                    Empty ->
-                        Nothing
-
-                    Occupied stone ->
-                        Just stone
-            )
-
-
-isOccupied : Board -> Stone -> Coord -> Bool
-isOccupied board stone coord =
-    cellAt board coord == Just (Occupied stone)
-
-
-cellAt : Board -> Coord -> Maybe Cell
-cellAt board { row, col } =
-    Array.get row board
-        `Maybe.andThen` (Array.get col)
-
-
-move : ( Int, Int ) -> Coord -> Coord
-move ( dx, dy ) { row, col } =
-    { row = row + dx, col = col + dy }
-
-
-setStones : Board -> Stone -> List Coord -> Board
-setStones board stone =
-    List.foldl (setStone stone) board
-
-
-setStone : Stone -> Coord -> Board -> Board
-setStone stone { row, col } board =
-    let
-        updateRow oldRow =
-            Array.set col (Occupied stone) oldRow
-    in
-        case Array.get row board of
-            Just oldRow ->
-                Array.set row (updateRow oldRow) board
-
-            Nothing ->
-                board
 
 
 init : Model
@@ -188,7 +80,7 @@ update msg model =
 
                 coords ->
                     ( { model
-                        | board = setStones model.board model.player (coord :: coords)
+                        | board = setStones model.board model.player coords
                         , player = other model.player
                         , highlighted = Nothing
                       }
@@ -250,9 +142,9 @@ renderCell highlighted coord cell =
                         , SvgAttr.r "4.5"
                         , SvgAttr.fill
                             (if highlighted then
-                                "#77cf77"
+                                "#88efa8"
                              else
-                                "#88a888"
+                                "#77a777"
                             )
                         ]
                         []
---
+++ Othello.elm
@@ -0,0 +1,126 @@
+module Othello
+    exposing
+        ( Board
+        , Cell(..)
+        , Stone(..)
+        , Coord
+        , other
+        , cellAt
+        , setStones
+        , isValidMove
+        )
+
+import Array exposing (Array)
+import Array as Array
+
+
+type alias Board =
+    Array (Array Cell)
+
+
+type Cell
+    = Empty
+    | Occupied Stone
+
+
+type Stone
+    = Black
+    | White
+
+
+type alias Coord =
+    { row : Int, col : Int }
+
+
+other : Stone -> Stone
+other stone =
+    case stone of
+        Black ->
+            White
+
+        White ->
+            Black
+
+
+cellAt : Board -> Coord -> Maybe Cell
+cellAt board { row, col } =
+    Array.get row board
+        `Maybe.andThen` (Array.get col)
+
+
+setStones : Board -> Stone -> List Coord -> Board
+setStones board stone =
+    List.foldl (setStone stone) board
+
+
+setStone : Stone -> Coord -> Board -> Board
+setStone stone { row, col } board =
+    let
+        updateRow oldRow =
+            Array.set col (Occupied stone) oldRow
+    in
+        case Array.get row board of
+            Just oldRow ->
+                Array.set row (updateRow oldRow) board
+
+            Nothing ->
+                board
+
+
+isValidMove : Board -> Stone -> Coord -> List Coord
+isValidMove board stone coord =
+    case
+        List.concatMap
+            (isValidMoveInDirection board stone coord)
+            [ ( -1, -1 ), ( -1, 0 ), ( -1, 1 ), ( 0, 1 ), ( 1, 1 ), ( 1, 0 ), ( 1, -1 ), ( 0, -1 ) ]
+    of
+        [] ->
+            []
+
+        cells ->
+            coord :: cells
+
+
+isValidMoveInDirection : Board -> Stone -> Coord -> ( Int, Int ) -> List Coord
+isValidMoveInDirection board stone coord dir =
+    if not (isEmpty board coord) then
+        []
+    else
+        terminatedWith board stone dir (move dir coord) []
+
+
+terminatedWith : Board -> Stone -> ( Int, Int ) -> Coord -> List Coord -> List Coord
+terminatedWith board stone dir coord captured =
+    case occupied board coord of
+        Just stone' ->
+            if stone' == stone then
+                captured
+            else
+                terminatedWith board stone dir (move dir coord) (coord :: captured)
+
+        Nothing ->
+            []
+
+
+isEmpty : Board -> Coord -> Bool
+isEmpty board coord =
+    cellAt board coord == Just Empty
+
+
+occupied : Board -> Coord -> Maybe Stone
+occupied board coord =
+    cellAt board coord
+        `Maybe.andThen`
+            (\cell ->
+                case cell of
+                    Empty ->
+                        Nothing
+
+                    Occupied stone ->
+                        Just stone
+            )
+
+
+move : ( Int, Int ) -> Coord -> Coord
+move ( dx, dy ) { row, col } =
+    { row = row + dx, col = col + dy }
